729866afee3baed48782a3ec604c4fc8251fd3e9
John Doe <johndoe@example.com>
2024-10-01 14:48:25 +0300

    fix seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+
 import type { Metadata } from "next";
-import "./globals.css";
 import dynamic from 'next/dynamic';
+import "./globals.css";
 
 const Footer = dynamic(() => import('@/components/Footer/Footer'), { ssr: false });
 
@@ -19,27 +20,22 @@ export const metadata: Metadata = {
     "ton developer",
     "accounting automation",
     "cost optimization",
-    "how to create your own game on the TON network",
     "blockchain developers",
   ],
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  // Проверяем, есть ли keywords и не равны ли они null или undefined
-  const keywordsContent =
-    metadata.keywords && Array.isArray(metadata.keywords)
-      ? metadata.keywords.join(", ")
-      : metadata.keywords || undefined;
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const keywordsContent = Array.isArray(metadata.keywords)
+  ? metadata.keywords.join(", ")
+  : metadata.keywords;
 
   return (
     <html lang="en">
+      
       <head>
-        <meta name="description" content={metadata.description || ''}/>
-        {keywordsContent && <meta name="keywords" content={keywordsContent} />}
+        <title>{'IcornA'}</title>
+        <meta name="description" content='Update your business now!' />
+        <meta name="keywords" content={keywordsContent || ""} />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-TV7R0P6EDQ"></script>
         <script
           dangerouslySetInnerHTML={{
