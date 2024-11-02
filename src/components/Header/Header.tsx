daca9848c91a50957fd8b84d2390f1b598c7c89a
John Doe <johndoe@example.com>
2024-11-02 12:23:38 +0200

    edits build

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState, useEffect, useRef } from 'react';
+import Image from 'next/image';
 import logo from '@/image/logo.svg';
 import './header.scss';
 
@@ -20,19 +21,14 @@ const Header: React.FC<HeaderProps> = ({
     automationRef,
     handleOpen
 }) => {
-    const [modalOpen, setModalOpen] = useState(false);
     const [isScrolled, setIsScrolled] = useState(false);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const menuRef = useRef<HTMLDivElement>(null);
     const logoRef = useRef<HTMLDivElement>(null);
-    const handleClose = () => setModalOpen(false);
+
     useEffect(() => {
         const handleScroll = () => {
-            if (window.scrollY > 50) {
-                setIsScrolled(true);
-            } else {
-                setIsScrolled(false);
-            }
+            setIsScrolled(window.scrollY > 50);
         };
 
         const handleClickOutside = (event: MouseEvent) => {
@@ -40,12 +36,12 @@ const Header: React.FC<HeaderProps> = ({
                 menuRef.current && !menuRef.current.contains(event.target as Node) &&
                 logoRef.current && !logoRef.current.contains(event.target as Node)
             ) {
-                setIsMenuOpen(false); // Закрываем меню при клике вне его области
+                setIsMenuOpen(false);
             }
         };
 
         window.addEventListener('scroll', handleScroll);
-        window.addEventListener('mousedown', handleClickOutside); // Слушаем клик вне меню
+        window.addEventListener('mousedown', handleClickOutside);
 
         return () => {
             window.removeEventListener('scroll', handleScroll);
@@ -53,16 +49,14 @@ const Header: React.FC<HeaderProps> = ({
         };
     }, []);
 
-    // Toggle the mobile menu (open or close when clicking logo)
     const toggleMenu = () => {
-        setIsMenuOpen(prevState => !prevState); // Меняем состояние на противоположное
+        setIsMenuOpen(prevState => !prevState);
     };
 
-    // Функция для плавного скролла к секции
     const handleScrollToSection = (ref: React.RefObject<HTMLDivElement>) => {
         if (ref.current) {
             ref.current.scrollIntoView({ behavior: 'smooth' });
-            setIsMenuOpen(false); // Закрываем меню после клика на ссылку
+            setIsMenuOpen(false);
         }
     };
 
@@ -70,9 +64,11 @@ const Header: React.FC<HeaderProps> = ({
         <>
             <div className={`header ${isScrolled ? 'header--scrolled' : ''} ${isMenuOpen ? 'header--menu-open' : ''}`}>
                 <div className="header__logo" ref={logoRef} onClick={toggleMenu}>
-                    <img
+                    <Image
                         src={logo.src}
                         alt="IcornA"
+                        width={50} // Укажите ширину изображения
+                        height={50} // Укажите высоту изображения
                     />
                 </div>
                 <nav ref={menuRef} className={`header__links ${isScrolled ? 'header--scrolled' : ''} ${isMenuOpen ? 'header__links--open' : ''}`}>
@@ -81,14 +77,12 @@ const Header: React.FC<HeaderProps> = ({
                     <a className="header__link" onClick={() => handleScrollToSection(designRef)}>Design</a>
                     <a className="header__link" onClick={() => handleScrollToSection(automationRef)}>Automation</a>
                     <button className="contact-button" onClick={handleOpen}>
-                    <div className="corner-top"></div>
-                    <div className="corner-bottom"></div>
-                    Get Free Audit
-                </button>
+                        <div className="corner-top"></div>
+                        <div className="corner-bottom"></div>
+                        Get Free Audit
+                    </button>
                 </nav>
-                
             </div>
-            {/* Overlay layer for blur effect */}
             {isMenuOpen && <div className="overlay overlay--visible" />}
         </>
     );
